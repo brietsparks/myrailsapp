4c0ee478843cd10f28b2ac1662db3c71aa5ec8cc
bsapaka <bws46@cornell.edu>
2017-03-19 23:38:27 -0500

    first commit

---
+++ config/application.rb
@@ -0,0 +1,20 @@
+require_relative 'boot'
+
+require 'rails/all'
+require 'neo4j/railtie'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Resumapp
+  class Application < Rails::Application
+
+      # ...
+      # config.generators { |g| g.orm :neo4j }
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
