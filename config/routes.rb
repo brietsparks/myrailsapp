d1eeaf011831552ec7176ea464f7cf55f2eceba4
bsapaka <bws46@cornell.edu>
2017-03-22 19:11:15 -0500

    added jwt auth login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  post 'auth_user' => 'authentication#authenticate_user'
+  get 'home' => 'home#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
